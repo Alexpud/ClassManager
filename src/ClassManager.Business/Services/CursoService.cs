f1b73b48c2058d11bd1b3c0de295572d60ff67cf
Alexandre Santos Maciel <alexandre.maciele@gmail.com>
2023-12-17 14:44:19 -0300

    Testes unitários pelo TDD

--- src/ClassManager.Business/Services/CursoService.cs
+++ src/ClassManager.Business/Services/CursoService.cs
@@ -1,19 +1,31 @@
 ﻿using ClassManager.Business.Dtos.Curso;
+using ClassManager.Business.Enums;
+using ClassManager.Business.Interfaces.Repositories;
 using ClassManager.Business.Notifications;
 
 namespace ClassManager.Business.Services;
 
 public class CursoService : BaseService
 {
-    public CursoService(INotificationServce notificationServce) : base(notificationServce)
+    private readonly IUsuarioRepository _usuarioRepository;
+    public CursoService(
+        INotificationServce notificationServce,
+        IUsuarioRepository usuarioRepository) : base(notificationServce)
     {
+        _usuarioRepository = usuarioRepository;
     }
 
-    public void AdicionarCurso(CriarCursoDto dto)
+    public async Task<CursoDto> CriarCurso(CriarCursoDto dto)
     {
+        if (dto.ProfessorId == Guid.Empty)
+            _notificationService.Handle("Não pode criar curso sem professor");
+
+        var usuario = await _usuarioRepository.ObterPorId(dto.ProfessorId);
+        if (usuario == null || usuario.Tipo != TipoUsuario.Professor)
+            _notificationService.Handle("Professor não foi encontrado");
         // Verificar se o curso é valido
-            // Possui nome?
-            // O professor existe?
-        
+        // Possui nome?
+        // O professor existe?
+        return new CursoDto();
     }
 }
---
+++ tests/ClassManager.Tests/Services/CursoServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using ClassManager.Business.Dtos.Curso;
+using ClassManager.Business.Entities;
+using ClassManager.Business.Enums;
+using ClassManager.Business.Interfaces.Repositories;
+using ClassManager.Business.Notifications;
+using ClassManager.Business.Services;
+using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using NSubstitute.ReturnsExtensions;
+
+namespace ClassManager.Business.Tests.Services;
+
+public class CursoServiceTests
+{
+    private readonly CursoService _sut;
+    private readonly INotificationServce _notificationService;
+    private readonly IUsuarioRepository _usuarioRepository;
+    public CursoServiceTests()
+    {
+        _notificationService = Substitute.For<INotificationServce>();
+        _usuarioRepository = Substitute.For<IUsuarioRepository>();
+        _sut = new CursoService(_notificationService, _usuarioRepository);
+    }
+
+    [Fact(DisplayName = "Criar Curso sem Professor deve falhar")]
+    [Trait("Categoria", "Curso")]
+    public async Task CriarCurso_SemProfessor_DeveFalhar()
+    {
+        // Arrange
+        
+        //  Act
+        await _sut.CriarCurso(new CriarCursoDto());
+
+        // Assert
+        _notificationService.Received().Handle(Arg.Any<string>());
+    }
+
+    [Trait("Categoria", "Curso")]
+    [Fact(DisplayName = "Criar Curso com professor que não existe deve falhar")]
+    public async Task CriarCurso_ProfessorNaoExiste_DeveFalhar()
+    {
+        // Arrange
+        var dto = new CriarCursoDto
+        {
+            ProfessorId = Guid.NewGuid(),
+        };
+        _usuarioRepository.ObterPorId(dto.ProfessorId)
+            .ReturnsNull();
+        // Act
+         await _sut.CriarCurso(dto);
+
+        // Assert
+        _notificationService.Received().Handle(Arg.Any<string>());
+    }
+
+    [Trait("Categoria", "Curso")]
+    [Fact(DisplayName = "Criar Curso com usuario que não é professor deve falhar")]
+    public async Task CriarCurso_UsuarioNaoProfessor_DeveFalhar()
+    {
+        // Arrange
+        var dto = new CriarCursoDto
+        {
+            ProfessorId = Guid.NewGuid(),
+        };
+        _usuarioRepository.ObterPorId(dto.ProfessorId)
+            .Returns(new Usuario()
+            {
+                Tipo = TipoUsuario.Aluno
+            });
+
+        // Act
+        await _sut.CriarCurso(dto);
+
+        // Assert
+        _notificationService.Received().Handle(Arg.Any<string>());
+    }
+}
